b465e323e7818683f0e37d97656a5664dccdb855
Biniyam Ajaw <91191700+biniyam69@users.noreply.github.com>
2024-07-17 14:26:27 +0100

    Add files via upload

---
+++ embed.js
@@ -0,0 +1,9 @@
+(function() {
+    const script = document.createElement('script');
+    script.src = 'https://risingstarstutors.com/assets/js/hotel-chatbot.js';
+    script.onload = () => {
+      window.renderChatWidgetBizai('chat-widget-container');
+    };
+    document.head.appendChild(script);
+  })();
+  

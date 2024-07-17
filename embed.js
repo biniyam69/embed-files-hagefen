8983637c8d0c1d809d6072231320ad84c7881e84
Biniyam Ajaw <91191700+biniyam69@users.noreply.github.com>
2024-07-17 14:27:47 +0100

    Update embed.js

--- embed.js
+++ embed.js
@@ -1,9 +1,8 @@
 (function() {
     const script = document.createElement('script');
-    script.src = 'https://risingstarstutors.com/assets/js/index-CMxK7HQS.js';
+    script.src = 'https://biniyam69.github.io/embed-files/index-CMxK7HQS.js';
     script.onload = () => {
       window.renderChatWidgetBizai('chat-widget-container');
     };
     document.head.appendChild(script);
   })();
-  

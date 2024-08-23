bd10aae1252db11ece6c0d9bf58630d153c7824b
Biniyam Ajaw <91191700+biniyam69@users.noreply.github.com>
2024-08-23 08:25:10 +0000

    Update embed.js

--- embed.js
+++ embed.js
@@ -1,6 +1,6 @@
 (function() {
     const script = document.createElement('script');
-    script.src = 'https://biniyam69.github.io/embed-files-hagefen/index-C9jtjX-P.js';
+    script.src = 'https://biniyam69.github.io/embed-files-hagefen/index-BGF-x76_.js';
     script.onload = () => {
       window.renderChatWidgetBizai('chat-widget-container');
     };
